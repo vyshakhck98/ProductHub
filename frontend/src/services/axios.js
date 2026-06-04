d117a26fdd0b9898ab945747985f360545f044ef
vyshakh <vyshakhck98@gmail.com>
2026-06-04 07:57:47 +0530

    changes on axios

--- frontend/src/services/axios.js
+++ frontend/src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://producthub-t6gs.onrender.com/api",
+  baseURL: "http://localhost:5000/api",
 });
 
 api.interceptors.request.use(
